b1da91bb25108254656b0e23d443306f00b4ba6f
Muthulakshmi-2003 <kamali1308kamali@gmail.com>
2026-09-25 20:24:02 +0530

    Add files via upload

---
+++ src/Practices/day25-09-2026/LeetCodeProblems/MergeSortedArray.java
@@ -0,0 +1,34 @@
+package Practices.day25092026.LeetCodeProblems;
+
+public class MergeSortedArray {
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = 0;
+        int j = 0;
+        int[] result = new int[m + n];
+        int k = 0;
+        while (i < m && j < n) {
+            if (nums1[i] <= nums2[j]) {
+                result[k] = nums1[i];
+                i++;
+            } else {
+                result[k] = nums2[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < m) {
+            result[k] = nums1[i];
+            i++;
+            k++;
+        }
+        while (j < n) {
+            result[k] = nums2[j];
+            j++;
+            k++;
+        }
+        for (int x = 0; x < result.length; x++) {
+            nums1[x] = result[x];
+        }
+    }
+}
